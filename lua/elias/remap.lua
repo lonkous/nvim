87c4a8f03078cc2ea60cff58bc50036d785b9179
Elias Kupeczki <github@kunoxy.ch>
2023-03-26 14:43:11 +0200

    added ctrl s

--- lua/elias/remap.lua
+++ lua/elias/remap.lua
@@ -1,6 +1,7 @@
 --makes leader to be space
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n","<C-s>", vim.cmd.w)
 --move highlighted stuff up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
