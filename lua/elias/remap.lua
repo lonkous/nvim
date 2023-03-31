2a911c37d788f6aa3ba999d662a9afa36a460448
eliasrk <github@kunoxy.ch>
2023-03-31 17:13:54 +0200

    remapped J and K

--- lua/elias/remap.lua
+++ lua/elias/remap.lua
@@ -2,8 +2,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 --move highlighted stuff up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<C-J>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-K>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 --jump up and down
 vim.keymap.set("n", "<C-j>", "<C-j>zz")
