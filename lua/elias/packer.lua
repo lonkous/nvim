a685f1ab39813b8e32eea9a92b22ff34c4c9d8fb
Elias Kupeczki <github@kunoxy.ch>
2023-04-23 13:15:55 +0200

    bootstrap

--- lua/elias/packer.lua
+++ lua/elias/packer.lua
@@ -1,53 +1,62 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
-
-
     -----------------------Base-----------------------
 
 
-    use ('wbthomason/packer.nvim') -- package manager
+    use('wbthomason/packer.nvim')                                 -- package manager
 
-    use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}) -- treesitter
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }) -- treesitter
 
-    use ('romgrk/nvim-treesitter-context') -- context
+    use('romgrk/nvim-treesitter-context')                         -- context
 
     use {
         "nvim-telescope/telescope-file-browser.nvim",
         requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-    } -- file browser
+    }                                 -- file browser
 
-    use ('nvim-treesitter/playground')  -- treesitter playground
+    use('nvim-treesitter/playground') -- treesitter playground
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     } -- telescope
 
 
     ----------------------Themes----------------------
 
 
-    use { 
+    use {
         "catppuccin/nvim",
-        as ="catppuccin",
+        as = "catppuccin",
         config = function()
             vim.cmd.colorscheme 'catppuccin-mocha'
         end,
-    } -- color
+    }                                          -- color
 
-    use ("lukas-reineke/indent-blankline.nvim") -- indent line
+    use("lukas-reineke/indent-blankline.nvim") -- indent line
 
     use {
         'nvim-tree/nvim-tree.lua',
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
         },
-        tag = 'nightly' 
-    } -- file tree
+        tag = 'nightly'
+    }                                     -- file tree
 
-    use {'nvim-tree/nvim-web-devicons'} -- icons
+    use { 'nvim-tree/nvim-web-devicons' } -- icons
 
-    use ({
+    use({
         "utilyre/barbecue.nvim",
         tag = "*",
         requires = {
@@ -57,9 +66,9 @@ return require('packer').startup(function(use)
         config = function()
             require("barbecue").setup()
         end,
-    }) -- route to file
+    })                                                           -- route to file
 
-    use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'} -- tabline
+    use { 'romgrk/barbar.nvim', requires = 'nvim-web-devicons' } -- tabline
 
     use {
         'nvim-lualine/lualine.nvim',
@@ -70,10 +79,10 @@ return require('packer').startup(function(use)
     ---------------------Tools---------------------
 
 
-    use ('prettier/vim-prettier') -- formatter
+    use('prettier/vim-prettier') -- formatter
 
     vim.api.nvim_create_autocmd('BufWritePost', {
-        pattern = {"*.js","*.ts", "*.jsx","*.tsx"},
+        pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
         callback = function()
             vim.cmd('Prettier')
         end,
@@ -86,17 +95,17 @@ return require('packer').startup(function(use)
             require("todo-comments").setup {
             }
         end
-    } -- todo comments
+    }                               -- todo comments
 
-    use ('mbbill/undotree') -- undo tree
+    use('mbbill/undotree')          -- undo tree
 
-    use ('tpope/vim-fugitive') -- git
+    use('tpope/vim-fugitive')       -- git
 
-    use ('github/copilot.vim') -- copilot
+    use('github/copilot.vim')       -- copilot
 
-    use ('RRethy/vim-illuminate') -- highlight word under cursor
-    
-    use ('HiPhish/nvim-ts-rainbow2') -- rainbow parentheses
+    use('RRethy/vim-illuminate')    -- highlight word under cursor
+
+    use('HiPhish/nvim-ts-rainbow2') -- rainbow parentheses
 
     use {
         'numToStr/Comment.nvim',
@@ -107,83 +116,84 @@ return require('packer').startup(function(use)
 
     use {
         'laytan/tailwind-sorter.nvim',
-        requires = {'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim'},
+        requires = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
         config = function() require('tailwind-sorter').setup() end,
         run = 'cd formatter && npm i && npm run build',
     } -- tailwind class sorter
 
-use {
-    "windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-} -- auto pairs
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    } -- auto pairs
 
-use {
-    "folke/trouble.nvim",
-    requires = "nvim-tree/nvim-web-devicons",
-    config = function()
-        require("trouble").setup {}
-    end
-} -- shows errors
-use('j-hui/fidget.nvim')
-require"fidget".setup{}
+    use {
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {}
+        end
+    } -- shows errors
+    use('j-hui/fidget.nvim')
+    require "fidget".setup {}
 
-use('onsails/lspkind-nvim')
+    use('onsails/lspkind-nvim')
 
--- install without yarn or npm
-use({
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
-})
+    -- install without yarn or npm
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
 
-use("ethanholz/nvim-lastplace")
+    use("ethanholz/nvim-lastplace")
 
-use("onsails/lspkind.nvim")
-
-
-
-------------------Help---------------------
+    use("onsails/lspkind.nvim")
 
 
-use {
-    "folke/which-key.nvim",
-    config = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-        require("which-key").setup {}
-    end
-} -- shows keybindings
 
-------------------Git---------------------
+    ------------------Help---------------------
 
 
-use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' } --Diffview
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {}
+        end
+    } -- shows keybindings
+
+    ------------------Git---------------------
 
 
-------------------LSP---------------------
+    use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' } --Diffview
 
 
-use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {'williamboman/mason.nvim'},           -- Optional
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    ------------------LSP---------------------
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},         -- Required
-        {'hrsh7th/cmp-nvim-lsp'},     -- Required
-        {'hrsh7th/cmp-buffer'},       -- Optional
-        {'hrsh7th/cmp-path'},         -- Optional
-        {'saadparwaiz1/cmp_luasnip'}, -- Optional
-        {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},             -- Required
-        {'rafamadriz/friendly-snippets'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },             -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
     }
-}
-
-  end)
-
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
