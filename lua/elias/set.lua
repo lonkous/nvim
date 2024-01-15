4b866e791591b33ca6cc5cbcbb4f030b03756084
eliasrk <github@kunoxy.ch>
2024-01-15 13:42:14 +0100

    updated LSP

--- lua/elias/set.lua
+++ lua/elias/set.lua
@@ -4,11 +4,11 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 
-vim.opt.softtabstop = 2
+vim.opt.softtabstop = 4
 
 vim.opt.expandtab = true
 
