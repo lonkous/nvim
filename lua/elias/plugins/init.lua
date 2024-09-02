ccee419d8c9deb19f2168e74d2020b9554524f19
lonkous <github@kunoxy.ch>
2024-09-02 12:00:12 +0200

    update scroll

--- lua/elias/plugins/init.lua
+++ lua/elias/plugins/init.lua
@@ -1,182 +1,188 @@
 return {
-    { "lukas-reineke/indent-blankline.nvim" },
-    { 'echasnovski/mini.animate' },
-    {
-        "utilyre/barbecue.nvim",
-        name = "barbecue",
-        version = "*",
-        dependencies = {
-            "SmiteshP/nvim-navic",
-            "nvim-tree/nvim-web-devicons", -- optional dependency
-        },
-        opts = {
-            -- configurations go here
-        },
-    },
-    { 'echasnovski/mini.indentscope' },
-    {
-        "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("todo-comments").setup {
-            }
-        end
-    },
-    { 'mbbill/undotree',
-        vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle) },
-    { 'tpope/vim-fugitive' },
-    { 'HiPhish/nvim-ts-rainbow2' },
-    {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    },
-    {
-        "folke/trouble.nvim",
-        requires = "nvim-tree/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {
-                mode = "document_diagnostics",
-            }
-        end
-    },
-    { "j-hui/fidget.nvim" },
-    {
-        "kdheepak/lazygit.nvim",
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-    },
-    { 'f-person/git-blame.nvim' },
-    {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-            require("which-key").setup {}
-        end
-    },
-    { "junegunn/fzf",         build = "./install --bin" },
-    {
-        "ibhagwan/fzf-lua",
-        -- optional for icon support
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            -- calling `setup` is optional for customization
-            require('fzf-lua').setup({
-                grep = {
-                    -- Using ripgrep with forced color output
-                    prompt         = '🔎',
-                    grep_opts      = "--color=always --line-number --column --no-heading",
-                    border         = 'rounded',
-                    preview_border = 'rounded',
-                    preview_wrap   = 'nowrap',
-                    preview_layout = 'flex',
+	{ "lukas-reineke/indent-blankline.nvim" },
+	{ "echasnovski/mini.animate" },
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		opts = {
+			-- configurations go here
+		},
+	},
+	{ "echasnovski/mini.indentscope" },
+	{
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({})
+		end,
+	},
+	{ "mbbill/undotree", vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) },
+	{ "tpope/vim-fugitive" },
+	{ "HiPhish/nvim-ts-rainbow2" },
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		requires = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				mode = "document_diagnostics",
+			})
+		end,
+	},
+	{ "j-hui/fidget.nvim" },
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{ "f-person/git-blame.nvim" },
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({})
+		end,
+	},
+	{ "junegunn/fzf", build = "./install --bin" },
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			-- calling `setup` is optional for customization
+			require("fzf-lua").setup({
+				grep = {
+					-- Using ripgrep with forced color output
+					prompt = "🔎",
+					grep_opts = "--color=always --line-number --column --no-heading",
+					border = "rounded",
+					preview_border = "rounded",
+					preview_wrap = "nowrap",
+					preview_layout = "flex",
+				},
+			})
+		end,
+	},
+	{ "nvim-lua/plenary.nvim" },
+	{
+		"j-hui/fidget.nvim",
+		dependencies = { "rcarriga/nvim-notify" },
+		opts = {},
+	},
+	{ "farmergreg/vim-lastplace" },
+	{
+		"rmagatti/auto-session",
+		config = function()
+			require("auto-session").setup({
+				log_level = "error",
+				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			})
+		end,
+	},
+	{
+		"SirVer/ultisnips",
+		config = function()
+			local ls = require("luasnip")
+			ls.add_snippets("javascript", {
 
-                },
-            })
-        end
-    }
+				ls.parser.parse_snippet("cc", "console.log(${1:variable})"),
+			})
+			ls.add_snippets("python", {
+				ls.parser.parse_snippet("lo", "_logger.info(${1})"),
+			})
+			ls.filetype_extend("typescriptreact", { "javascriptreact" })
+			ls.filetype_extend("typescript", { "javascript" })
+			ls.filetype_extend("javascript", { "html" })
+			ls.filetype_extend("xml", { "html" })
+			ls.filetype_extend("typescriptreact", { "javascript" })
+		end,
+	},
+	{ "github/copilot.vim" },
+	{ "romgrk/nvim-treesitter-context" },
+	{ "nvim-treesitter/nvim-treesitter-textobjects" },
+	{ "RRethy/vim-illuminate" },
+	{
+		"zbirenbaum/neodim",
+		event = "LspAttach",
+		config = function()
+			require("neodim").setup({
+				refresh_delay = 75,
+				alpha = 0.50,
+				blend_color = "#10171f",
+				hide = {
+					underline = true,
+					virtual_text = true,
+					signs = true,
+				},
+				regex = {
+					"[uU]nused",
+					"[nN]ever [rR]ead",
+					"[nN]ot [rR]ead",
+				},
+				priority = 128,
+				disable = {},
+			})
+		end,
+	},
+	{
+		"karb94/neoscroll.nvim",
+		config = function()
+			neoscroll = require("neoscroll")
+			local keymap = {
+				["<C-k>"] = function()
+					neoscroll.scroll(-50, {
+						move_cursor = true,
+						duration = 150,
+						easing = "easeInOutQuint",
+					})
+				end,
+				["<C-j>"] = function()
+					neoscroll.scroll(50, {
+						move_cursor = true,
+						duration = 150,
+						easing = "easeInOutQuint",
+					})
+				end,
+			}
+			local modes = { "n", "v", "x" }
+			for key, func in pairs(keymap) do
+				vim.keymap.set(modes, key, func)
+			end
+		end,
+	},
+	{ "windwp/nvim-ts-autotag" },
+	{
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({
+				render = "background",
+				enable_tailwind = true,
+			})
+		end,
+	},
+	{
 
-    ,
-    { 'nvim-lua/plenary.nvim' },
-    {
-        "j-hui/fidget.nvim",
-        dependencies = { "rcarriga/nvim-notify" },
-        opts = {},
-    },
-    { 'farmergreg/vim-lastplace' }
-    , {
-    'rmagatti/auto-session',
-    config = function()
-        require("auto-session").setup {
-            log_level = "error",
-            auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-        }
-    end
-},
-    {
-        "SirVer/ultisnips",
-        config = function()
-            local ls = require("luasnip")
-            ls.add_snippets("javascript", {
-
-                ls.parser.parse_snippet(
-                    "cc",
-                    "console.log(${1:variable})"
-                ),
-
-            })
-            ls.add_snippets("python", {
-                ls.parser.parse_snippet(
-                    "lo",
-                    "_logger.info(${1})"
-                )
-            })
-            ls.filetype_extend("typescriptreact", { "javascriptreact" })
-            ls.filetype_extend("typescript", { "javascript" })
-            ls.filetype_extend("javascript", { "html" })
-            ls.filetype_extend("xml", { "html" })
-            ls.filetype_extend("typescriptreact", { "javascript" })
-        end
-    },
-    { "github/copilot.vim" },
-    { 'romgrk/nvim-treesitter-context' },
-    { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    { 'RRethy/vim-illuminate' },
-    {
-        "zbirenbaum/neodim",
-        event = "LspAttach",
-        config = function()
-            require("neodim").setup({
-                refresh_delay = 75,
-                alpha = 0.50,
-                blend_color = "#10171f",
-                hide = {
-                    underline = true,
-                    virtual_text = true,
-                    signs = true,
-                },
-                regex = {
-                    "[uU]nused",
-                    "[nN]ever [rR]ead",
-                    "[nN]ot [rR]ead",
-                },
-                priority = 128,
-                disable = {},
-            })
-        end
-    },
-    {
-        "karb94/neoscroll.nvim",
-        config = function()
-            require('neoscroll').setup { mappings = {}
-            }
-            local t = {}
-            -- Syntax: t[keys] = {function, {function arguments}}
-            t['<C-k>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '350', [['sine']] } }
-            t['<C-j>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '350', [['sine']] } }
-            require('neoscroll.config').set_mappings(t)
-        end
-    },
-    { "windwp/nvim-ts-autotag" },
-    {
-        'brenoprata10/nvim-highlight-colors',
-        config = function()
-            require('nvim-highlight-colors').setup({
-                render = 'background',
-                enable_tailwind = true,
-            })
-        end
-    }, {
-
-    "mfussenegger/nvim-lint", },
-    { "rshkarin/mason-nvim-lint", },
+		"mfussenegger/nvim-lint",
+	},
+	{ "rshkarin/mason-nvim-lint" },
 }
--- lua/elias/plugins/lsp.lua
+++ lua/elias/plugins/lsp.lua
@@ -170,15 +170,19 @@ return {
 				-- auto-install configured servers (with lspconfig)
 				automatic_installation = true, -- not the same as ensure_installed
 			})
+			local lspconfig = require("lspconfig")
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			mason_lspconfig.setup_handlers({
-				function(server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup({})
-				end,
+			local lsp_attach = function(client, bufnr)
+				-- Create your keybindings here...
+			end
+
+			require("mason-lspconfig").setup_handlers({
 				function(server_name)
-					--
-					-- Default handler for all other servers
-					lspconfig[server_name].setup({})
+					lspconfig[server_name].setup({
+						on_attach = lsp_attach,
+						capabilities = lsp_capabilities,
+					})
 				end,
 			})
 		end,
