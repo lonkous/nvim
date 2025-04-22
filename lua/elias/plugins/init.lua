bcaea672753fe7348ea4e7683de7eb3bfc7769b2
lonkous <github@kunoxy.ch>
2025-04-22 13:06:31 +0200

    remove barbecue and added winbar

--- lua/elias/plugins/init.lua
+++ lua/elias/plugins/init.lua
@@ -1,125 +1,113 @@
 return {
-    { "lukas-reineke/indent-blankline.nvim" },
-    { "echasnovski/mini.animate" },
-    {
-        "utilyre/barbecue.nvim",
-        name = "barbecue",
-        version = "*",
-        dependencies = {
-            "SmiteshP/nvim-navic",
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-        },
-    },
-    {
-        "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("todo-comments").setup({})
-        end,
-    },
-    { "mbbill/undotree",
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) },
-    { "tpope/vim-fugitive" },
-    {
-        'windwp/nvim-autopairs',
-        event = 'InsertEnter',
-        -- Optional dependency
-        dependencies = { 'hrsh7th/nvim-cmp' },
-        config = function()
-            require('nvim-autopairs').setup {}
-            -- If you want to automatically add `(` after selecting a function or method
-            local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-            local cmp = require 'cmp'
-            cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-        end,
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-    },
-    { "f-person/git-blame.nvim" },
-    {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-            require("which-key").setup({})
-        end,
-    },
-    {
-        "j-hui/fidget.nvim",
-        dependencies = { "rcarriga/nvim-notify" },
-        opts = {},
-    },
-    { "farmergreg/vim-lastplace" },
-    {
-        "rmagatti/auto-session",
-        config = function()
-            require("auto-session").setup({
-                log_level = "error",
-                auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-            })
-        end,
-    },
-    { "github/copilot.vim" },
-    { "romgrk/nvim-treesitter-context" },
-    { "nvim-treesitter/nvim-treesitter-textobjects" },
-    { "RRethy/vim-illuminate" },
-    {
-        "zbirenbaum/neodim",
-        event = "LspAttach",
-        config = function()
-            require("neodim").setup({
-                refresh_delay = 75,
-                alpha = 0.50,
-                blend_color = "#10171f",
-                hide = {
-                    underline = true,
-                    virtual_text = true,
-                    signs = true,
-                },
-                regex = {
-                    "[uU]nused",
-                    "[nN]ever [rR]ead",
-                    "[nN]ot [rR]ead",
-                },
-                priority = 128,
-                disable = {},
-            })
-        end,
-    },
-    {
-        "windwp/nvim-ts-autotag",
-        config = function()
-            require('nvim-ts-autotag').setup({
-                opts = {
-                    enable_close = true,
-                    enable_rename = true,
-                    enable_close_on_slash = false
-                },
-            })
-        end
-    },
-    {
-        "brenoprata10/nvim-highlight-colors",
-        config = function()
-            require("nvim-highlight-colors").setup({
-                render = "background",
-                enable_tailwind = true,
-            })
-        end,
-    },
-    { 'tpope/vim-sleuth' },
-    { 'tpope/vim-speeddating' },
+	{ "lukas-reineke/indent-blankline.nvim" },
+	{ "echasnovski/mini.animate" },
+	{
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({})
+		end,
+	},
+	{ "mbbill/undotree", vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) },
+	{ "tpope/vim-fugitive" },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		-- Optional dependency
+		dependencies = { "hrsh7th/nvim-cmp" },
+		config = function()
+			require("nvim-autopairs").setup({})
+			-- If you want to automatically add `(` after selecting a function or method
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{ "f-person/git-blame.nvim" },
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		dependencies = { "rcarriga/nvim-notify" },
+		opts = {},
+	},
+	{ "farmergreg/vim-lastplace" },
+	{
+		"rmagatti/auto-session",
+		config = function()
+			require("auto-session").setup({
+				log_level = "error",
+				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			})
+		end,
+	},
+	{ "github/copilot.vim" },
+	{ "romgrk/nvim-treesitter-context" },
+	{ "nvim-treesitter/nvim-treesitter-textobjects" },
+	{ "RRethy/vim-illuminate" },
+	{
+		"zbirenbaum/neodim",
+		event = "LspAttach",
+		config = function()
+			require("neodim").setup({
+				refresh_delay = 75,
+				alpha = 0.50,
+				blend_color = "#10171f",
+				hide = {
+					underline = true,
+					virtual_text = true,
+					signs = true,
+				},
+				regex = {
+					"[uU]nused",
+					"[nN]ever [rR]ead",
+					"[nN]ot [rR]ead",
+				},
+				priority = 128,
+				disable = {},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = false,
+				},
+			})
+		end,
+	},
+	{
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({
+				render = "background",
+				enable_tailwind = true,
+			})
+		end,
+	},
+	{ "tpope/vim-sleuth" },
+	{ "tpope/vim-speeddating" },
 }
