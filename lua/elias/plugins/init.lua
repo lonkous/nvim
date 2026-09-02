44f3efd20d6cff4962e3e08557c41e29a508ef34
Lonkous <github@kunoxy.ch>
2026-09-02 18:00:50 +0200

    treesitter context fix

--- lua/elias/plugins/init.lua
+++ lua/elias/plugins/init.lua
@@ -62,7 +62,22 @@ return {
 			-- log_level = 'debug',
 		},
 	},
-	{ "romgrk/nvim-treesitter-context" },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		branch = "master",
+		lazy = false,
+		opts = {
+			enable = true,
+			max_lines = 3,
+			min_window_height = 0,
+			multiwindow = true,
+			multiline_threshold = 20,
+			trim_scope = "outer",
+			mode = "cursor",
+			separator = nil,
+			zindex = 20,
+		},
+	},
 	{ "nvim-treesitter/nvim-treesitter-textobjects" },
 	{ "RRethy/vim-illuminate" },
 	{
